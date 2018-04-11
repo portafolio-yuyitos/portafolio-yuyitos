a1e03e677a121e752a24aeec6d07348849213e53
JoseZunigaDyehs <zuniga.joseignacio@gmail.com>
2018-04-11 01:49:06 -0300

    ordn pedido

--- client/assets/js/orden-pedido.js
+++ client/assets/js/orden-pedido.js
@@ -1,7 +1,41 @@
+function valCantidad(e,min,max) {
+  let valor = e.value;
+  var reg = new RegExp('^[0-9]+$');
+  var error = $(e).siblings('.error');
+  var valido = true;
+
+  if (valor.trim() === "") {
+    error.text('No debe estar vacío');
+    error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
+  } else if (!reg.test(valor)) {
+    e.value = valor.substring(0, valor.length - 1)//quitar ultimo caracter
+    error.text('Ingrese sólo números');
+    error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
+  } else if (valor < min) {
+    error.text('Mínimo: ' + min);
+    error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
+  } else if (valor > max) {
+    error.text('Máximo: ' + max);
+    error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
+  } else {
+    error.addClass('d-none');
+    $(e).removeClass('is-invalid');
+  }
+  return valido;
+}
+
 function validarTodo() {
   var proveedor = $('#proveedor');
   var producto = $('#producto');
-  var cantidad = $('#cantidad');
+  var cantidad = document.getElementById('cantidad');
 
   var valido = true;
 
@@ -11,14 +45,16 @@ function validarTodo() {
   if (!valSelect(producto)) {
     valido = false;
   }
-  if (!valNumber(numero, 1, 6)) {
+  if (!valCantidad(cantidad, 1, 99999999)) {
     valido = false;
   }
 
   if (valido) {
     var productos = $('#productosContainer').find('ul');
-    productos.append('<li>'+producto.val()+'</li>')
-  } else {
+    productos.append('<li>'+producto.val()+'</li>');
+    proveedor['0'].value = "Seleccione";
+    producto['0'].value = "Seleccione";
+    cantidad.value = 0;
     return valido;
   }
 }
--- client/assets/js/proveedores.js
+++ client/assets/js/proveedores.js
@@ -1,4 +1,4 @@
-function validarTodo(){
+function validarTodo() {
   var nombre = $('#nombre');
   var numero = document.getElementById('numero');
   var email = $('#email');
@@ -6,57 +6,57 @@ function validarTodo(){
 
   var valido = true;
 
-  if(!valTexto(nombre, 4, 50)){
-    valido = false;  
+  if (!valTexto(nombre, 4, 50)) {
+    valido = false;
   }
-  if(!valNumber(numero, 5, 12)){
-    valido = false;  
+  if (!valNumber(numero, 5, 12)) {
+    valido = false;
   }
-  if(!valEmail(email)){
-    valido = false;  
+  if (!valEmail(email)) {
+    valido = false;
   }
-  if(!valTexto(rubro, 4, 50)){
-    valido = false;  
+  if (!valTexto(rubro, 4, 50)) {
+    valido = false;
   }
 
-  if(valido){
-      return proveedor = {
-        "nombre": nombre.val(),
-        "numero": numero.value,
-        "email": email.val(),
-        "rubro": rubro.val()
-      }
-  }else{
+  if (valido) {
+    return proveedor = {
+      "nombre": nombre.val(),
+      "numero": numero.value,
+      "email": email.val(),
+      "rubro": rubro.val()
+    }
+  } else {
     return valido;
   }
 }
 
-function llenarTabla(proveedor){
+function llenarTabla(proveedor) {
   debugger;
   var tabla = $('#tabla');
   var largoTabla = tabla.find('tbody tr').length;
   var fila = '<tr>';
 
   fila += '<tr>';
-  fila += '<th scope="row">'+(largoTabla+1)+'</th>';
+  fila += '<th scope="row">' + (largoTabla + 1) + '</th>';
   fila += '<td>';
-  fila += '<p>'+proveedor.nombre+'</p>';
-  fila += '<input type="text" value='+proveedor.nombre+' class="form-control editar d-none" onkeyup="valTexto(this,4,50)">';
+  fila += '<p>' + proveedor.nombre + '</p>';
+  fila += '<input type="text" value=' + proveedor.nombre + ' class="form-control editar d-none" onkeyup="valTexto(this,4,50)">';
   fila += '<label class="error text-danger d-none "></label>';
   fila += '</td>';
   fila += '<td>';
-  fila += '<p>'+proveedor.numero+'</p>';
-  fila += '<input type="text" value='+proveedor.numero+' class="form-control editar d-none" onkeyup="valNumber(this,9,12)">';
+  fila += '<p>' + proveedor.numero + '</p>';
+  fila += '<input type="text" value=' + proveedor.numero + ' class="form-control editar d-none" onkeyup="valNumber(this,9,12)">';
   fila += '<label class="error text-danger d-none "></label>';
   fila += '</td>';
   fila += '<td>';
-  fila += '<p>'+proveedor.email+'</p>';
-  fila += '<input type="text" value='+proveedor.email+' class="form-control editar d-none" onkeyup="valEmail(this)">';
+  fila += '<p>' + proveedor.email + '</p>';
+  fila += '<input type="text" value=' + proveedor.email + ' class="form-control editar d-none" onkeyup="valEmail(this)">';
   fila += '<label class="error text-danger d-none "></label>';
   fila += '</td>';
   fila += '<td>';
-  fila += '<p>'+proveedor.rubro+'</p>';
-  fila += '<input type="text" value='+proveedor.rubro+' class="form-control editar d-none" onkeyup="valTexto(this,4,50)">';
+  fila += '<p>' + proveedor.rubro + '</p>';
+  fila += '<input type="text" value=' + proveedor.rubro + ' class="form-control editar d-none" onkeyup="valTexto(this,4,50)">';
   fila += '<label class="error text-danger d-none "></label>';
   fila += '</td>';
   fila += '<td>';
