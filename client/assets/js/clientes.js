f91a925b8fa81f3667a44cd50a5c15b784a318cc
JoseZunigaDyehs <zuniga.joseignacio@gmail.com>
2018-04-10 21:05:21 -0300

    se suben cambios, clientes está ok

--- client/assets/js/clientes.js
+++ client/assets/js/clientes.js
@@ -1,10 +1,8 @@
 function eliminarCliente(e) {
-  debugger
   $(e).closest('tr').remove();
 }
 
 function editarCliente(e) {
-  debugger
   var editores = $(e).closest('tr').find('.editar');
   $.each(editores, function (i, editor) {
     //Si es la ultima columna
@@ -20,7 +18,6 @@ function editarCliente(e) {
 }
 
 function guardarCliente(e) {
-  debugger
   var editores = $(e).closest('tr').find('.editar');
   var valido = true;
 
@@ -29,6 +26,7 @@ function guardarCliente(e) {
     if (editores.length - 1 !== i) {
       if (!$(editor).siblings('.error').hasClass('d-none')) {
         valido = false;
+        alert('Debe ingresar todos los campos válidos');
         return false;
       }
     }
@@ -49,4 +47,98 @@ function guardarCliente(e) {
       }
     })
   }
+}
+
+function validarTodo(){
+
+  var rut = $('#rut');
+  var nombres = $('#nombres');
+  var apellidos = $('#apellidos');
+  var email = $('#email');
+  var numero = $('#numero');
+
+  var valido = true;
+
+  if(!valTexto(rut, 10, 10)){
+    valido = false;  
+  }
+  if(!valTexto(nombres, 10, 10)){
+    valido = false;  
+  }
+  if(!valTexto(apellidos, 10, 10)){
+    valido = false;  
+  }
+  if(!valTexto(email, 10, 20)){
+    valido = false;  
+  }
+  if(!valTexto(numero, 10, 10)){
+    valido = false;  
+  }
+
+  if(valido){
+      return cliente = {
+        "rut":rut.val(),
+        "nombres": nombres.val(),
+        "apellidos": apellidos.val(),
+        "email": email.val(),
+        "numero": numero.val()
+      }
+  }else{
+    return valido;
+  }
+}
+
+function agregarCliente(){
+
+  var valido = validarTodo();
+
+  if(!valido){
+    alert('Hay campos no válidos')
+    return false;
+  }else{
+    llenarTablaCliente(valido);
+    return true;
+  }
+}
+
+function llenarTablaCliente(cliente){
+  debugger;
+  var tabla = $('#tablaClientes');
+  var largoTabla = tabla.find('tbody tr').length;
+  var fila = '<tr>';
+  fila += '<th scope="row">'+(largoTabla+1)+'</th>';
+  fila += '<td>';
+  fila += '<p>'+cliente.rut+'</p>';
+  fila += '<input type="text" value="'+cliente.rut+'" class="form-control editar d-none" onkeyup="valTexto(this,10,20)">';
+  fila += '<label class="error text-danger d-none "></label>';
+  fila += '</td>';
+  fila += '<td>';
+  fila += '<p>'+cliente.nombres+'</p>';
+  fila += '<input type="text" value="'+cliente.nombres+'" class="form-control editar d-none" onkeyup="valTexto(this,10,20)">';
+  fila += '<label class="error text-danger d-none "></label>';
+  fila += '</td>';
+  fila += '<td>';
+  fila += '<p>'+cliente.apellidos+'</p>';
+  fila += '<input type="text" value="'+cliente.apellidos+'" class="form-control editar d-none" onkeyup="valTexto(this,10,20)">';
+  fila += '<label class="error text-danger d-none "></label>';
+  fila += '</td>';
+  fila += '<td>';
+  fila += '<input type="text" value="'+cliente.email+'" class="form-control editar d-none">';
+  fila += '<label class="error text-danger d-none "></label>';
+  fila += '<p>'+cliente.email+'</p>';
+  fila += '</td>';
+  fila += '<td>';
+  fila += '<p>'+cliente.numero+'</p>';
+  fila += '<input type="text" value="'+cliente.numero+'" class="form-control editar d-none" onkeyup="valNumber(this,5,10)">';
+  fila += '<label class="error text-danger d-none "></label>';
+  fila += '</td>';
+  fila += '<td class="d-flex">';
+  fila += '<a  class="btn btn-primary editar d-none cursor-pointer" onclick="guardarCliente(this)" data-toggle="tooltip" data-placement="top" title="Guardar cliente"><i class="fas fa-check-circle"></i></a>';
+  fila += '<a  class="btn btn-danger btnEliminar mr-2 cursor-pointer" onclick="eliminarCliente(this)" data-toggle="tooltip" data-placement="top" title="Eliminar cliente"><i class="fas fa-trash-alt"></i></a>';
+  fila += '<a  class="btn btn-secondary btnEditar cursor-pointer" onclick="editarCliente(this)" data-toggle="tooltip" data-placement="top" title="Editar cliente"><i class="fas fa-edit"></i></a>';
+  fila += '</td>';
+  fila += '</tr>';
+
+  tabla.find('tbody').append(fila);
+  alert('Se ha agregado el cliente');
 }
--- client/assets/js/general.js
+++ client/assets/js/general.js
@@ -12,38 +12,128 @@ $(document).ready(function(){
 function valTexto(e, min, max) {
   let valor = $(e).val();
   var error = $(e).siblings('.error');
+  var valido = true;
+
   if (valor.trim() === "") {
     error.text('No debe estar vacío');
     error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
   } else if (valor.length < min) {
     error.text('Mínimo de caracteres: ' + min);
     error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
   } else if (valor.length > max) {
     error.text('Máximo de caracteres: ' + max);
     error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
   } else {
     error.addClass('d-none');
+    $(e).removeClass('is-invalid');
   }
+  return valido;
 }
 
 function valNumber(e, min, max) {
   let valor = e.value;
   var reg = new RegExp('^[0-9]+$');
   var error = $(e).siblings('.error');
+  var valido = true;
+
   if (valor.trim() === "") {
     error.text('No debe estar vacío');
     error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
   } else if (!reg.test(valor)) {
     e.value = valor.substring(0, valor.length - 1)//quitar ultimo caracter
     error.text('Ingrese sólo números');
     error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
   } else if (valor.length < min) {
     error.text('Mínimo de caracteres: ' + min);
     error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
   } else if (valor.length > max) {
     error.text('Máximo de caracteres: ' + max);
     error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+    valido = false;
   } else {
     error.addClass('d-none');
+    $(e).removeClass('is-invalid');
   }
+  return valido;
+}
+
+function valEmail(e){
+  var valor = $(e).val();
+  var error = $(e).siblings('.error');
+
+  if (!valor) {
+    error.text('No debe estar vacío');
+    error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(valor)) {
+    error.text('Debe ingresar email válido');
+    error.removeClass('d-none');
+    $(e).addClass('is-invalid');
+  }else{
+    error.addClass('d-none');
+    $(e).removeClass('is-invalid');
+  }
+}
+
+function checkRut(rut) {
+    //debugger;
+    // Despejar Puntos
+    var valor = rut.value.replace('.','');
+    // Despejar Guión
+    valor = valor.replace('-','');
+    
+    // Aislar Cuerpo y Dígito Verificador
+    cuerpo = valor.slice(0,-1);
+    dv = valor.slice(-1).toUpperCase();
+    
+    // Formatear RUN
+    rut.value = cuerpo + '-'+ dv
+    
+    // Si no cumple con el mínimo ej. (n.nnn.nnn)
+    if(cuerpo.length < 7) { console.log("RUT Incompleto"); return false;}
+    
+    // Calcular Dígito Verificador
+    suma = 0;
+    multiplo = 2;
+    
+    // Para cada dígito del Cuerpo
+    for(i=1;i<=cuerpo.length;i++) {
+    
+        // Obtener su Producto con el Múltiplo Correspondiente
+        index = multiplo * valor.charAt(cuerpo.length - i);
+        
+        // Sumar al Contador General
+        suma = suma + index;
+        
+        // Consolidar Múltiplo dentro del rango [2,7]
+        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+  
+    }
+    
+    // Calcular Dígito Verificador en base al Módulo 11
+    dvEsperado = 11 - (suma % 11);
+    
+    // Casos Especiales (0 y K)
+    dv = (dv == 'K')?10:dv;
+    dv = (dv == 0)?11:dv;
+    
+    // Validar que el Cuerpo coincide con su Dígito Verificador
+    if(dvEsperado != dv) { console.log("RUT Inválido"); return false; }
+    
+    // Si todo sale bien, eliminar errores (decretar que es válido)
+    rut.setCustomValidity('');
+    console.log('RUT VÁLIDO')
 }
